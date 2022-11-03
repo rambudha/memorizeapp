6f38d770c84d79542308cc252ba76cd76460309f
rambudha <devops.rambudha@gmail.com>
2022-11-04 10:58:12 +1100

    Initial Commit

---
+++ memorizeappl/memorizeapplApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeapplApp.swift
+//  memorizeappl
+//
+//  Created by Ryamsey Ryam on 4/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct memorizeapplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
